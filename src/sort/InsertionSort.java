44ae611ea2a6d3f699ee73174fdf4b8a858a0973
liqi328 <liqi328@163.com>
2013-06-03 15:49:03 +0800

    完成插入排序

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -1,6 +1,26 @@
 package sort;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 	public static void sort(int[] array){
+		int tmp, j;
+		for(int i = 1; i < array.length; ++i){
+			j = i;
+			tmp = array[j];
+			while(j > 0 && tmp < array[j - 1]){
+				array[j] = array[j - 1];
+				--j;
+			}
+			array[j] = tmp;
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		int[] array = {49, 38, 65, 97, 76, 13, 27, 49};
+		System.out.println(Arrays.toString(array));
+		sort(array);
+		System.out.println(Arrays.toString(array));
 	}
 }
